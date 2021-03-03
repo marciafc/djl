6566b41becee3a4e5511507dd854b80f3223e434
Marcia Castagna <marcia.castagna@hypeflame.com.br>
2021-03-03 01:48:04 -0300

    Criando projeto

---
+++ src/main/java/br/com/marcia/ai/djl/DjlApplication.java
@@ -0,0 +1,65 @@
+package br.com.marcia.ai.djl;
+
+import ai.djl.inference.Predictor;
+import ai.djl.modality.cv.ImageFactory;
+
+import ai.djl.modality.cv.output.DetectedObjects;
+import ai.djl.translate.TranslateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import ai.djl.modality.cv.Image;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class DjlApplication implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DjlApplication.class);
+
+	@Resource
+	private Supplier<Predictor<Image, DetectedObjects>> predictorProvider;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		var imagens = new org.springframework.core.io.Resource[] {
+				new ClassPathResource("/como-tirar-foto-de-cachorro.jpg"),
+				new ClassPathResource("/1307576.jpg"),
+				new ClassPathResource("/693450.jpg"),
+				new ClassPathResource("/marcia1.png"),
+				new ClassPathResource("/marcia2.jpg"),
+				new ClassPathResource("/marcia3.jpg"),
+
+		};
+
+		for(var imagem: imagens) {
+			rodarInferencia(imagem);
+		}
+	}
+
+	private void rodarInferencia(org.springframework.core.io.Resource imagem) throws IOException, TranslateException {
+		Image image = ImageFactory.getInstance().fromInputStream(imagem.getInputStream());
+
+		try (var predictor = predictorProvider.get()) {
+			var resultados = predictor.predict(image);
+
+			var items = resultados.items();
+
+			for (var resultado : items) {
+				LOG.info("Resultados para {}: {}", imagem.getFilename(), resultado.toString());
+			}
+		}
+	}
+}
---
+++ src/test/java/br/com/marcia/ai/djl/DjlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcia.ai.djl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
